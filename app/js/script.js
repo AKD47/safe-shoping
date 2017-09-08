efbfd8ce8bc7a4ff17b37863f0c4d52672760a5f
AKD47 <van83horn@gmail.com>
2017-09-08 16:05:16 +0300

    product slider fin

--- app/js/script.js
+++ app/js/script.js
@@ -24,4 +24,39 @@ $(document).ready(function () {
         return false;
     });
     /*close*/
+    
+    /*product slider*/
+    $('.product-slider__carousel').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 660,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }            
+        ]
+    });
+    /*close*/
 });
