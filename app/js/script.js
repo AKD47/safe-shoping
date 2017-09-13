7974c360b8c7e1b7d1bf043a2e9f0971056c75b8
AKD47 <van83horn@gmail.com>
2017-09-13 21:14:44 +0300

    start respons

--- app/js/script.js
+++ app/js/script.js
@@ -31,13 +31,13 @@ $(document).ready(function () {
         infinite: false,
         speed: 300,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 1,
                     infinite: true,
                     dots: true
                 }
@@ -46,7 +46,7 @@ $(document).ready(function () {
                 breakpoint: 770,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
                 }
             },
             {
